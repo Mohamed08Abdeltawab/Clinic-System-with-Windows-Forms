7ce4112f570791c07094f370720477e552a2a992
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-02-23 00:18:05 +0200

    add ctrlUserCard and test it well

---
+++ Clinic/User/ctrlUserCard.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Clinicbusiness;
+
+namespace Clinic.User
+{
+    public partial class ctrlUserCard : UserControl
+    {
+        clsUser _User;
+        private int _UserID = -1;
+
+        //useing UserID in forms 
+        public int UserID
+        {
+            get {  return _UserID; }
+        }
+        public ctrlUserCard()
+        {
+            InitializeComponent();
+        }
+
+
+        public void LoadUserInfo(int PersonID)
+        {
+            _User = clsUser.FindByPersonID(PersonID);
+            _UserID = _User.UserID;
+            if (_User == null)
+            {
+                _ResetUserInfo();
+                MessageBox.Show("Error, user is not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            _FillUserInfo();
+        }
+
+        private void _FillUserInfo()
+        {
+            ctrlPersonCard1.LoadInfo(_User.PersonID);
+            lblUserID.Text = _User.UserID.ToString();
+            lblUserName.Text = _User.UserName.ToString();
+            lblRole.Text = _User.RoleName.ToString();
+        }
+
+        private void _ResetUserInfo()
+        {
+            ctrlPersonCard1.ResetPersonInfo();
+            lblUserID.Text = "[???]";
+            lblUserName.Text = "[???]";
+            lblRole.Text = "[???]";
+        }
+    }
+}
---
+++ Clinic/User/frmUserInfo.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Clinic.User
+{
+    partial class frmUserInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ctrlUserCard1 = new Clinic.User.ctrlUserCard();
+            this.SuspendLayout();
+            // 
+            // ctrlUserCard1
+            // 
+            this.ctrlUserCard1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ctrlUserCard1.Location = new System.Drawing.Point(7, 36);
+            this.ctrlUserCard1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ctrlUserCard1.Name = "ctrlUserCard1";
+            this.ctrlUserCard1.Size = new System.Drawing.Size(839, 404);
+            this.ctrlUserCard1.TabIndex = 0;
+            // 
+            // frmUserInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 511);
+            this.Controls.Add(this.ctrlUserCard1);
+            this.Name = "frmUserInfo";
+            this.Text = "frmUserInfo";
+            this.Load += new System.EventHandler(this.frmUserInfo_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ctrlUserCard ctrlUserCard1;
+    }
+}
