69e8c564a1f0e91fc961eb80840f0d8412d6526f
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-02-23 00:29:21 +0200

    add frmUserInfo and test it well

--- Clinic/User/frmUserInfo.Designer.cs
+++ Clinic/User/frmUserInfo.Designer.cs
@@ -28,24 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnClose = new System.Windows.Forms.Button();
             this.ctrlUserCard1 = new Clinic.User.ctrlUserCard();
             this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClose.Image = global::Clinic.Properties.Resources.Close_32;
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClose.Location = new System.Drawing.Point(720, 411);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(126, 37);
+            this.btnClose.TabIndex = 17;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // ctrlUserCard1
             // 
             this.ctrlUserCard1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ctrlUserCard1.Location = new System.Drawing.Point(7, 36);
+            this.ctrlUserCard1.Location = new System.Drawing.Point(7, -3);
             this.ctrlUserCard1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.ctrlUserCard1.Name = "ctrlUserCard1";
             this.ctrlUserCard1.Size = new System.Drawing.Size(839, 404);
-            this.ctrlUserCard1.TabIndex = 0;
+            this.ctrlUserCard1.TabIndex = 18;
             // 
             // frmUserInfo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(859, 511);
+            this.ClientSize = new System.Drawing.Size(859, 462);
             this.Controls.Add(this.ctrlUserCard1);
+            this.Controls.Add(this.btnClose);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmUserInfo";
             this.Text = "frmUserInfo";
             this.Load += new System.EventHandler(this.frmUserInfo_Load);
@@ -55,6 +75,7 @@
 
         #endregion
 
+        private System.Windows.Forms.Button btnClose;
         private ctrlUserCard ctrlUserCard1;
     }
 }
--- Clinic/User/frmUserInfo.cs
+++ Clinic/User/frmUserInfo.cs
@@ -12,14 +12,21 @@ namespace Clinic.User
 {
     public partial class frmUserInfo : Form
     {
-        public frmUserInfo()
+        private int _UserID = -1;
+        public frmUserInfo(int UserID)
         {
             InitializeComponent();
+            _UserID = UserID;
         }
 
         private void frmUserInfo_Load(object sender, EventArgs e)
         {
-            ctrlUserCard1.LoadUserInfo(1);
+            ctrlUserCard1.LoadUserInfo(_UserID);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
