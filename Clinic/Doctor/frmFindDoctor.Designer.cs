2b540c632592132318d303db62a7c885a7a74b3e
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-02-26 01:23:36 +0200

    add frmFindDodctor

---
+++ Clinic/Doctor/frmFindDoctor.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Clinic.Doctor
+{
+    partial class frmFindDoctor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ctrlDoctorInfo1 = new Clinic.Doctor.ctrlDoctorInfo();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ctrlDoctorInfo1
+            // 
+            this.ctrlDoctorInfo1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ctrlDoctorInfo1.Location = new System.Drawing.Point(4, 14);
+            this.ctrlDoctorInfo1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ctrlDoctorInfo1.Name = "ctrlDoctorInfo1";
+            this.ctrlDoctorInfo1.Size = new System.Drawing.Size(842, 453);
+            this.ctrlDoctorInfo1.TabIndex = 0;
+            // 
+            // btnClose
+            // 
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClose.Image = global::Clinic.Properties.Resources.Close_32;
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClose.Location = new System.Drawing.Point(702, 459);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(126, 37);
+            this.btnClose.TabIndex = 17;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmFindDoctor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 509);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.ctrlDoctorInfo1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmFindDoctor";
+            this.Text = "frmFindDoctor";
+            this.Load += new System.EventHandler(this.frmFindDoctor_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ctrlDoctorInfo ctrlDoctorInfo1;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ Clinic/Doctor/frmFindDoctor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clinic.Doctor
+{
+    public partial class frmFindDoctor : Form
+    {
+        private int _DoctorID;
+        public frmFindDoctor(int DoctorID)
+        {
+            InitializeComponent();
+            _DoctorID = DoctorID;
+        }
+
+        private void frmFindDoctor_Load(object sender, EventArgs e)
+        {
+            ctrlDoctorInfo1.LoadDoctorInfo(_DoctorID);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
