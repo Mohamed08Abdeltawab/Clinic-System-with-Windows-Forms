48f11217133126d32a09e9543f314d55002ae46c
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-03-07 01:04:06 +0200

    Add class AppointmentType in data and buisness layer

---
+++ ClinicData/clsAppointmentTypeData.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicData
+{
+    public class clsAppointmentTypeData
+    {
+        public static bool GetAppointmentTypeInfoByID(int AppointmentTypeID,
+        ref string AppointmentTypeName, ref decimal AppointmentTypeFees)
+        {
+            bool isFound = false;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            // تم استخدام أسماء الأعمدة من جدولك: AppointmentType
+            string query = "SELECT * FROM AppointmentType WHERE AppointmentTypeID = @AppointmentTypeID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@AppointmentTypeID", AppointmentTypeID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    isFound = true;
+                    AppointmentTypeName = (string)reader["AppointmentTypeName"];
+                    // تحويل القيمة لـ decimal لضمان الدقة المالية
+                    AppointmentTypeFees = Convert.ToDecimal(reader["AppointmentTypeFees"]);
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                isFound = false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isFound;
+        }
+
+        // 2. دالة جلب جميع أنواع المواعيد (لتعبئة الـ ComboBox)
+        public static DataTable GetAllAppointmentTypes()
+        {
+            DataTable dt = new DataTable();
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "SELECT * FROM AppointmentType";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    dt.Load(reader);
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                // التعامل مع الخطأ
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return dt;
+        }
+
+        // 3. دالة تحديث السعر أو الاسم (اختياري لو أردت تعديل الأسعار من السيستم)
+        public static bool UpdateAppointmentType(int AppointmentTypeID,
+            string AppointmentTypeName, decimal AppointmentTypeFees)
+        {
+            int rowsAffected = 0;
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"UPDATE AppointmentType  
+                         SET AppointmentTypeName = @AppointmentTypeName, 
+                             AppointmentTypeFees = @AppointmentTypeFees
+                         WHERE AppointmentTypeID = @AppointmentTypeID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@AppointmentTypeID", AppointmentTypeID);
+            command.Parameters.AddWithValue("@AppointmentTypeName", AppointmentTypeName);
+            command.Parameters.AddWithValue("@AppointmentTypeFees", AppointmentTypeFees);
+
+            try
+            {
+                connection.Open();
+                rowsAffected = command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return (rowsAffected > 0);
+        }
+    }
+}
---
+++ Clinicbusiness/clsAppointmentType.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClinicData;
+
+namespace Clinicbusiness
+{
+    internal class clsAppointmentType
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.Update;
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Fees { get; set; }
+
+        // مشيد (Constructor) فارغ
+        public clsAppointmentType()
+        {
+            this.ID = -1;
+            this.Name = "";
+            this.Fees = 0;
+            Mode = enMode.AddNew;
+        }
+
+        // مشيد خاص لعملية الـ Find
+        private clsAppointmentType(int ID, string Name, decimal Fees)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Fees = Fees;
+            Mode = enMode.Update;
+        }
+
+        // 1. دالة البحث عن نوع معين
+        public static clsAppointmentType Find(int ID)
+        {
+            string Name = "";
+            decimal Fees = 0;
+
+            if (clsAppointmentTypeData.GetAppointmentTypeInfoByID(ID, ref Name, ref Fees))
+            {
+                return new clsAppointmentType(ID, Name, Fees);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        // 2. دالة جلب كل الأنواع لتعبئة الـ ComboBox
+        public static DataTable GetAllAppointmentTypes()
+        {
+            return clsAppointmentTypeData.GetAllAppointmentTypes();
+        }
+
+        // 3. دالة التحديث (خاصة)
+        private bool _Update()
+        {
+            return clsAppointmentTypeData.UpdateAppointmentType(this.ID, this.Name, this.Fees);
+        }
+
+        // 4. دالة الحفظ العامة
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    // يمكنك إضافة دالة AddNew في الـ Data Layer لو أردت إضافة أنواع جديدة برمجياً
+                    return false;
+                case enMode.Update:
+                    return _Update();
+            }
+            return false;
+        }
+    }
+}
