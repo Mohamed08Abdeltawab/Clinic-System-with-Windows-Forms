c26a264a394f2e86ee1e99c302f1ddb84d63835e
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-03-06 04:31:19 +0200

    add frmFindDoctor UI and Logic

---
+++ Clinic/Doctor/frmFindDoctor.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Clinic.Doctor
+{
+    partial class frmFindDoctor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.ctrlDoctorCardWithFilter1 = new Clinic.Doctor.ctrlDoctorCardWithFilter();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblTitle.Location = new System.Drawing.Point(265, 20);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(308, 39);
+            this.lblTitle.TabIndex = 122;
+            this.lblTitle.Text = "Find Doctor Info";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnClose
+            // 
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClose.Image = global::Clinic.Properties.Resources.Close_32;
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClose.Location = new System.Drawing.Point(697, 612);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(126, 37);
+            this.btnClose.TabIndex = 123;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // ctrlDoctorCardWithFilter1
+            // 
+            this.ctrlDoctorCardWithFilter1.FilterEnabled = true;
+            this.ctrlDoctorCardWithFilter1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ctrlDoctorCardWithFilter1.Location = new System.Drawing.Point(-3, 71);
+            this.ctrlDoctorCardWithFilter1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ctrlDoctorCardWithFilter1.Name = "ctrlDoctorCardWithFilter1";
+            this.ctrlDoctorCardWithFilter1.ShowAddDoctor = true;
+            this.ctrlDoctorCardWithFilter1.Size = new System.Drawing.Size(842, 531);
+            this.ctrlDoctorCardWithFilter1.TabIndex = 124;
+            // 
+            // frmFindDoctor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 668);
+            this.Controls.Add(this.ctrlDoctorCardWithFilter1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblTitle);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmFindDoctor";
+            this.Text = "frmFindDoctor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblTitle;
+        private ctrlDoctorCardWithFilter ctrlDoctorCardWithFilter1;
+    }
+}
---
+++ Clinic/Doctor/frmFindDoctor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clinic.Doctor
+{
+    public partial class frmFindDoctor : Form
+    {
+
+        public delegate void DataBackEventHandler(object sender, int DoctorID);
+        public event DataBackEventHandler DataBack;
+        public frmFindDoctor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            DataBack.Invoke(this, ctrlDoctorCardWithFilter1.DoctorID);
+            this.Close();
+        }
+    }
+}
